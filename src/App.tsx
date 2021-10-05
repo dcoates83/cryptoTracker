b0fd37028baa6282820f36150d5d5198a06811b0
Devon Coates <63134707+dcoates83@users.noreply.github.com>
2021-10-04 19:38:36 -0600

    add api call & export statements to files to stop eslint from yelling at me

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { getCoins } from './api/cryptoAPI';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function App() {
+  getCoins();
   return <div>Hello World</div>;
 }
 
---
+++ src/api/cryptoAPI.ts
@@ -0,0 +1,79 @@
+/* eslint-disable camelcase */
+export {};
+export interface TrendingCrypto {
+  id: string;
+  coin_id: number;
+  name: string;
+  symbol: string;
+  market_cap_rank: number;
+  thumb: string;
+  small: string;
+  large: string;
+  slug: string;
+  price_btc: number;
+  score: number;
+}
+
+export interface CryptoEntry {
+  id: string;
+  symbol: string;
+  name: string;
+  image: string;
+  current_price: number;
+  market_cap: number;
+  market_cap_rank: number;
+  fully_diluted_valuation: number;
+  total_volume: number;
+  high_24h: number;
+  low_24h: number;
+  price_change_24h: number;
+  price_change_percentage_24h: number;
+  market_cap_change_24h: number;
+  market_cap_change_percentage_24h: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  ath: number;
+  ath_change_percentage: number;
+  ath_date: string;
+  atl: number;
+  atl_change_percentage: number;
+  atl_date: string;
+  roi?: {
+    times: number;
+    currency: string;
+    percentage: number;
+  };
+  last_updated: string;
+}
+
+const default_coin_ids = [
+  'bitcoin',
+  'ethereum',
+  'binancecoin',
+  'tether',
+  'ripple',
+  'litecoin',
+  'chainlink',
+  'terra-luna',
+  'vechain',
+];
+
+const fetchWrapper = (url: string) => fetch(url, { mode: 'cors' }).then((resp) => {
+  if (!resp.ok) throw new Error(`Error: ${resp.status} - ${resp.statusText}`);
+  return resp.json();
+});
+
+export const getCoins = (coinIds?: string[]): Promise<CryptoEntry[]> => fetchWrapper(
+  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${(
+    coinIds || default_coin_ids
+  ).join()}`,
+).then((resp) => resp);
+
+export const getTrending = (): Promise<{
+  coins: [
+    {
+      item: TrendingCrypto;
+    },
+  ];
+}> => fetchWrapper('https://api.coingecko.com/api/v3/search/trending').then((resp) => resp);
